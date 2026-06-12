4f42797207a4e88a0576db0dcd92c945319162a6
Tjasa Spes <tjasa.spes@gmail.com>
2026-06-12 08:42:54 +0000

    [LeetCode Sync] Runtime - 22 ms (3.34%), Memory - 9.5 MB (5.04%)

--- problems/0014-longest-common-prefix/solution.go
+++ problems/0014-longest-common-prefix/solution.go
@@ -1,26 +1,24 @@
 func longestCommonPrefix(strs []string) string {
 
 	out := ""
-    if len(strs) == 1 {
-        return strs[0]
-    }
-
-	// check the size of the shortest word
-	max := 2000
-	for _, val := range strs {
-		if max > len(val) {
-			max = len(val)
-		}
+	if len(strs) == 1 {
+		return strs[0]
 	}
 
 	correct := true
 	var next rune
 
 	//going through letters of the word
-	for j := 0; j < max; j++ {
+	for j := 0; j < len(strs[0]); j++ {
 
 		//going throught all words for letter possition j
 		for i := 1; i <= len(strs)-1; i++ {
+
+            //checking for the shortest word
+			if len(strs[i]) <= j {
+                correct = false
+				break
+			}
 
 			if []rune(strs[i-1])[j] != []rune(strs[i])[j] {
 				correct = false
